2ff3c64cf08b80e5193974be3b5ca06362ec87c1
JFFI <juliofloresinteriano@gmail.com>
2015-07-05 12:33:19 -0600

    p

--- send.php
+++ send.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPConnection;
 use PhpAmqpLib\Message\AMQPMessage;
-$connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPConnection('192.168.1.16', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 $channel->queue_declare('hello', false, false, false, false);
 $msg = new AMQPMessage('Hello World!');
