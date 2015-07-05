0cf14a43872fb9bac19a213dc4f3eaa8dbe4b0e9
JFFI <juliofloresinteriano@gmail.com>
2015-07-05 09:44:19 -0600

    probandophp

---
+++ publish.php
@@ -0,0 +1,13 @@
+ <?php
+
+echo
+"<form action='' method='post'>
+<input type='submit' name='use_button' value='something' />
+</form>";
+
+if(isset($_POST['use_button']))
+{
+    echo "hey";
+}
+
+?> 
